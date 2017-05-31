4b1a85cc703051630006bd812c270a3579b5175f
bimik <bimik@o2.pl>
2017-05-31 09:45:45 +0200

    1.1

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@
 
 var a = prompt('Podaj a');
 var b = prompt('Podaj b');
-var value = (a * a) + (2 * a * b) - (b * b)
+var value = (a * a) + (2 * a * b) - (b * b);
 alert(value);
-console.log(' a: ' + a + ' and b: ' + b + ' is equal to: ' + value);
+console.log(' If a : ' + a + ' and b: ' + b + ' Then equation (a * a) + (2 * a * b) - (b * b) is equal to: ' + value);
 
 if ( value < 0) {
     console.log('Wartość ujemna');
